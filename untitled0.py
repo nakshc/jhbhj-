b4610a7a29e777461b1c2a6ff72a62f2d216acff
nakshc <72503643+nakshc@users.noreply.github.com>
2021-02-18 20:26:55 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,20 @@
+
+
+name="naksh" 
+print(type(name))
+
+blah=10
+print(type(blah))
+
+yo_man_this_is_a_list=["yo","bros","whassup","do","you","play","football"]
+print(yo_man_this_is_a_list)
+print(type(yo_man_this_is_a_list))
+
+from  tkinter  import   *
+
+root=Tk()
+root.title("C145")
+root.geometry("300x300")
+
+
+root.mainloop();
